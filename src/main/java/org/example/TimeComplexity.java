2401fa845ef7e2f9119b343d1b11d22845790dc0
Oğuzhan Akçay <oguzhanakcay90@gmail.com>
2024-04-04 01:14:32 +0300

    Gereksinimler karşılandı

---
+++ src/main/java/org/example/TimeComplexity.java
@@ -0,0 +1,30 @@
+package org.example;
+
+public class TimeComplexity {
+    public static void timeComplexity(int param) {
+
+        if (param > 4) {
+            System.out.println("dsgfshafsda");
+        }
+        if (param > 4 && param < 10) {
+            System.out.println("asdfsdghg");
+        }
+
+        for (int i = 0; i < param; i++) {
+            for (int y = 0; y < param; i++) {
+
+                System.out.println(param);
+            }
+        }
+
+
+        for (int i = 0; i < param; i++) {
+            for (int y = 0; y < param; i++) {
+
+                System.out.println(param);
+            }
+        }
+
+        // n^2
+    }
+}
